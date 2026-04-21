3d53319955cafabaef3fd790878e57b6359281a0
alezcsgo <126515990+alezcsgo@users.noreply.github.com>
2026-04-21 20:38:04 +0200

    Aktueller Stand

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/stream-show.js',
             ],
             refresh: true,
         }),
